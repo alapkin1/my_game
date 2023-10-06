095a74b45c2cfb969bfabfce7cb1e2224a65bf78
uwu <146203643+alapkin1@users.noreply.github.com>
2023-10-06 17:05:01 +0300

    Update full.py

--- full.py
+++ full.py
@@ -8,10 +8,12 @@ background = transform.scale(image.load('fon.jpg'), window_size)
 
 
 class GameSprite(sprite.Sprite): 
-    def __init__(self, player_image,x_size , y_size, x_cor, y_cor, speed):
+    def __init__(self, player_image,x_size , y_size, x_cor, y_cor, speed_x, speed_y, speed):
         super().__init__()
         self.image = transform.scale(image.load(player_image),(x_size, y_size))
         self.speed = speed
+        self.speed.x = speed_x
+        self.speed.y = speed_y
         self.rect = self.image.get_rect()
         self.rect.x = x_cor
         self.rect.y = y_cor
@@ -23,13 +25,24 @@ class Player(GameSprite):
     def update(self):
         keys_pressed = key.get_pressed()   
         if keys_pressed[K_a] and self.rect.x > 0:
+            self.rect.x -= self.speed
+        if keys_pressed[K_d] and self.rect.x < 300:
+            self.rect.x += self.speed 
+        if keys_pressed[K_w] and self.rect.y < 100:
+            self.rect.y += self.speed 
+        if keys_pressed[K_s] and self.rect.y > 600:
+            self.rect.y += self.speed  
+
+    def update2(self):
+        keys_pressed = key.get_pressed()   
+        if keys_pressed[K_a] and self.rect.x > 300:
             self.rect.x -= self.speed 
         if keys_pressed[K_d] and self.rect.x < 635:
             self.rect.x += self.speed 
         if keys_pressed[K_w] and self.rect.y < 100:
             self.rect.y += self.speed 
         if keys_pressed[K_s] and self.rect.y > 600:
-            self.rect.y += self.speed  
+            self.rect.y += self.speed         
 
 class Enemy(GameSprite):
     def update(self):
