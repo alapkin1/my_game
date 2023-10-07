880778636caf1919c653c4fe1970fb4ad7b6a1e3
uwu <146203643+alapkin1@users.noreply.github.com>
2023-10-07 22:55:57 +0300

    Update full.py

--- full.py
+++ full.py
@@ -12,7 +12,7 @@ display.set_caption('Volleyball')
 background2 = transform.scale(image.load('1.jpg'), window_size)
 
 class GameSprite(sprite.Sprite): 
-    def __init__(self, player_image,x_size , y_size, x_cor, y_cor, speed):
+    def __init__(self, player_image,x_size , y_size, x_cor, y_cor, speed, speed_x, speed_y):
         super().__init__()
         self.image = transform.scale(image.load(player_image),(x_size, y_size))
         self.speed = speed
@@ -23,12 +23,44 @@ class GameSprite(sprite.Sprite):
     def reset(self):
         window.blit(self.image,(self.rect.x, self.rect.y))
 
+class Ball(GameSprite):
+    def update(self):
+        self.rect.x += self.speed_x
+        self.rect.y += self.speed_y
+        if self.rect.y <= 0:
+            self.speed_y *= -1
+        if self.rect.y >= 460:
+            self.speed_y *= -1
+        if ball.rect.colliderect(player1.rect) or ball.rect.colliderect(player2.rect):
+            self.speed_x *= -1
+            self.speed_y *= 1
+
+class Button():
+    def __init__(self, x_cor, y_cor, x_size, y_size, image):
+        self.x_cor = x_cor
+        self.y_cor = y_cor
+        self.x_size = x_size
+        self.y_size = y_size
+        self.image = transform.scale(image.load(image),(x_size, y_size))
+
+    def showButton(self, display):
+        if udate == True:
+            py.draw.rect(display,(self.x_cor, self.y_cor,self.x_size, self.y_size, self.image))
+    
+    def focusCheck(self, mousepos, mouseclick):
+        if(mousepos[0] >= self.x and mousepos[0] <= self.x + self.sx and mousepos[1] >= self.y and mousepos[1] <= self.y + self.sy):
+            self.CurrentState = True
+            return mouseclick[0]
+        else:
+            udate = False
+
 
 font.init()  
 form = font.SysFont('Arial', 50)
 
 FPS = 60
 clock = time.Clock()
+udate = True
 finish = False
 game = True
 while game:
